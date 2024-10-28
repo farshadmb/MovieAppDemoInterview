02b2aa08141b9cbf540f5a52cb6774ea8c345fa4
Farshad Mousalou <farshadm90@gmail.com>
2024-10-28 03:33:43 +0330

    Add Movie entity

--- MovieDemoApp/Classes/Domain/Entities/Movie.swift
+++ MovieDemoApp/Classes/Domain/Entities/Movie.swift
@@ -9,4 +9,31 @@ import Foundation
 
 struct Movie {
     
+    let id: Int
+    let isAdult: Bool
+    let backdropPath: String?
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let popularity: Double
+    let posterPath: String?
+    let releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case isAdult = "adult"
+        case backdropPath
+        case originalLanguage
+        case originalTitle
+        case overview, popularity
+        case posterPath
+        case releaseDate
+        case title, video
+        case voteAverage
+        case voteCount
+    }
+    
 }
