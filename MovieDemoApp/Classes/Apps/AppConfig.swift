2570ea99f24d74d79d899f20c2ccfc4e507b8a39
Farshad Mousalou <farshadm90@gmail.com>
2024-10-29 13:16:17 +0330

    Fix

--- MovieDemoApp/Classes/Apps/AppConfig.swift
+++ MovieDemoApp/Classes/Apps/AppConfig.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct AppConfig {
     
-    static let ApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNzc1Nzk2MDJiMjU0YTcwYjhkNDkwNzc1N2YxOTcxMSIsIm5iZiI6MTczMDA4OTI4Ny41Njc5MzgsInN1YiI6IjY3MWYwZmEyMjY4NWNiNjU2M2MxNDE1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fDM_7N5cpmsxOjEOcZHXver1dnlnMb-danMt77KjRm0"
+    static let ApiKey = "YOUR-API-KEY"
     static let defaltPageSize = 10
     // swiftlint:disable:next force_unwrapping
     static let baseURL = URL(string: "https://api.themoviedb.org/3/")!
