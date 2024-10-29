95d6ad006bccd0482f3c66687497b6a3432172da
Farshad Mousalou <farshadm90@gmail.com>
2024-10-29 16:32:09 +0330

    Add movie detail repository function and fix movie repo adaptation

--- MovieDemoApp/Classes/Data/Repository/RemoteMoviesRepository.swift
+++ MovieDemoApp/Classes/Data/Repository/RemoteMoviesRepository.swift
@@ -12,6 +12,7 @@ import RxSwiftExt
 final class RemoteMoviesRepository: MoviesRepository {
     
     typealias MoviesResponse = GenericResponse<MovieListResponse>
+    typealias MovieDetailResponse = GenericResponse<MovieDetailDto>
     
     let apiClient: ApiClient
     let baseURL: URL
@@ -63,6 +64,41 @@ final class RemoteMoviesRepository: MoviesRepository {
                         switch response {
                         case .success(let result):
                             observer.onNext(result.data?.results.map { $0.toDomain() } ?? [])
+                            observer.onCompleted()
+                        case .failure(let error):
+                            observer.onError(error)
+                        }
+                    }
+                
+                return Disposables.create {
+                    task.cancel()
+                }
+            } catch let error {
+                observer.onError(error)
+            }
+            
+            return Disposables.create()
+        }
+        .asSingle()
+    }
+    
+    func movieDetails(forId id: Int) -> Single<Movie> {
+        Observable.create {[weak base = self] observer in
+            guard let self = base else {
+                return Disposables.create()
+            }
+            do {
+                let request = try MoviesRequestEndpoint.movieDetail(id: id)
+                    .request(baseURL: self.baseURL)
+                let task = try self.apiClient
+                    .sendRequest(request: request) { (response: Result<MovieDetailResponse, Error>) in
+                        switch response {
+                        case .success(let result):
+                            guard let data = result.data?.toDomain() else {
+                                observer.onError(URLError(.badServerResponse))
+                                return
+                            }
+                            observer.onNext(data)
                             observer.onCompleted()
                         case .failure(let error):
                             observer.onError(error)
